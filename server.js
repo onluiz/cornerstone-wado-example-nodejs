d603d9ac88e9570da491694a701a1b9e2b9cbc8c
onluiz <luiz.alberto1723@gmail.com>
2017-09-20 08:57:50 -0300

    ajustando url no server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 app.use('/', express.static(__dirname + '/public'));
 app.listen(3000, function() { 
     console.log('listening port 3000')
-    console.log('http://localhost:3000/cornerstoneWADOImageLoader/examples/')
+    console.log('http://localhost:3000/examples/')
 });
